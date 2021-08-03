d94da014afa6b68d89ce95dd0de1d7fbf0531379
labibkhanofficialbd <labibkhan66410@gmail.com>
2021-08-03 23:48:55 +0600

    recap-loop

---
+++ gpa.js
@@ -0,0 +1,22 @@
+
+var marks= 80;
+
+if (marks>=80 && marks<=100){
+    console.log("Your Grade is A+");
+}
+else if (marks>=70 && marks<=79){
+    console.log("Your Grade is A");
+}
+
+else if (marks>=60 && marks<=69){
+    console.log("Your Grade is A-");
+}
+else if (marks>=50 && marks<=59){
+    console.log("Your Grade is B");
+}
+else if (marks>=40 && marks<=49){
+    console.log("Your Grade is C")
+}    
+else{
+    console.log("fail");
+}
---
+++ while-loop.js
@@ -0,0 +1,8 @@
+
+
+var roastGiven = 0;
+
+while (roastGiven < 7){
+    console.log('Roast den!! please!!');
+    roastGiven++;
+}
