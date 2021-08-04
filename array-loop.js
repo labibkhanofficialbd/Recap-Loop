957e8a353474d5ebb007c9f2b210b9d9c5edade5
labibkhanofficialbd <labibkhan66410@gmail.com>
2021-08-04 11:50:18 +0600

    recap-loop

---
+++ array-loop.js
@@ -0,0 +1,14 @@
+
+// var numbers = [40, 65, 78, 98, 113, 78, 43, 54, 23, 65];
+
+// for(var i = 0; i< numbers.length; i++){
+//     var element = numbers[i];
+//     console.log(element);
+// }
+
+var items = ['bottle', 'cup', 'mug', 'glass', 'watch', 'pen'];
+
+for(var i=0; i<items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
